1342d22e60150f1f2c1daf22cbcc96c187aba900
jjg0202 <juan.gonzalez-flores@unt.edu>
2024-05-15 21:08:39 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -60,7 +60,7 @@ ambientSound.player.volume.value = -10; //turn down volume
 export var adventureMusic = new soundFile("PONG-Ambient Audio.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -16;
+adventureMusic.player.volume.value = -6;
 
 export var villageMusic = new soundFile("silence.mp3");
 soundArray.push(villageMusic);
