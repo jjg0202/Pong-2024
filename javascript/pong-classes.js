1430b8cbb0e43f7545e05cb0be51a16db84a27dc
jjg0202 <juan.gonzalez-flores@unt.edu>
2024-05-16 20:04:24 -0500

    Update pong-classes.js

--- javascript/pong-classes.js
+++ javascript/pong-classes.js
@@ -15,7 +15,7 @@ export class Game {
     computerDifficulty,
     computerDifficultySpeed
   ) {
-    this.size = { x: 2400, y: 900 }; //container for entire game
+    this.size = { x: 2000, y: 900 }; //container for entire game
     this.padding = { x: 10, y: 10 }; //white space inside game border
     this.htmlElement = null; //element on HTML page
     this.mousePosition = { x: 0, y: 0 };
